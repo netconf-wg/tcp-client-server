592407f90303c9e8999eddc23cb60589e5d8477c
kwatsen <kent@watsen.net>
2020-07-08 20:59:16 -0400

    this is -06

--- Makefile
+++ Makefile
@@ -37,9 +37,11 @@ idnits: $(next).txt
 
 clean:
 	-rm -f $(draft)-[0-9][0-9].xml
+	-rm -f $(draft)-[0-9][0-9].v2v3.xml
 	-rm -f $(draft)-[0-9][0-9].txt
 	-rm -f $(draft)-[0-9][0-9].html
 	-rm -f ietf-*\@20*.yang
+	-rm -f metadata.min.js
 ifeq (md,$(draft_type))
 	-rm -f $(draft).xml
 endif
@@ -54,7 +56,8 @@ $(next).xml: $(draft).xml ietf-tcp-client.yang ietf-tcp-server.yang
 	sed -e"s/YYYY-MM-DD/$(shell date +%Y-%m-%d)/" ietf-tcp-server.yang > ietf-tcp-server\@$(shell date +%Y-%m-%d).yang
 	cd refs && ./validate-all.sh && ./gen-trees.sh && cd ..
 	./.insert-figures.sh $@ > tmp && mv tmp $@
-	rm refs/*-tree*.txt
+	rm refs/*-tree*.txt refs/tree-*.txt
+	xml2rfc --v2v3 $@
 
 
 .INTERMEDIATE: $(draft).xml
@@ -65,10 +68,10 @@ $(next).xml: $(draft).xml ietf-tcp-client.yang ietf-tcp-server.yang
 	$(oxtradoc) -m outline-to-xml -n "$@" $< > $@
 
 %.txt: %.xml
-	$(xml2rfc) $< -o $@ --text
+	$(xml2rfc) --v3 $< -o $@ --text
 
 %.html: %.xml
-	$(xml2rfc) $< -o $@ --html
+	$(xml2rfc) --v3 $< -o $@ --html
 
 
 ### Below this deals with updating gh-pages
--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,10 +1,38 @@
+echo "Generating tree diagrams..."
 
-pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings ../ietf-tcp-client\@*.yang > ietf-tcp-client-tree.txt
-pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings ../ietf-tcp-server\@*.yang > ietf-tcp-server-tree.txt
-pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings ../ietf-tcp-common\@*.yang > ietf-tcp-common-tree.txt
+#pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings ../ietf-tcp-client\@*.yang > ietf-tcp-client-tree.txt
+#pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings ../ietf-tcp-server\@*.yang > ietf-tcp-server-tree.txt
+#pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings ../ietf-tcp-common\@*.yang > ietf-tcp-common-tree.txt
 
+#pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings --tree-no-expand-uses ../ietf-tcp-common\@*.yang > ietf-tcp-common-tree-no-expand.txt
+#pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings --tree-no-expand-uses ../ietf-tcp-client\@*.yang > ietf-tcp-client-tree-no-expand.txt
+#pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings --tree-no-expand-uses ../ietf-tcp-server\@*.yang > ietf-tcp-server-tree-no-expand.txt
 
-pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings --tree-no-expand-uses ../ietf-tcp-client\@*.yang > ietf-tcp-client-tree-no-expand.txt
-pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings --tree-no-expand-uses ../ietf-tcp-server\@*.yang > ietf-tcp-server-tree-no-expand.txt
-pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings --tree-no-expand-uses ../ietf-tcp-common\@*.yang > ietf-tcp-common-tree-no-expand.txt
+extract_grouping_with_params() {
+  # $1 name of module
+  # $2 name of grouping
+  # $3 addition CLI params
+  # $4 output filename
+  pyang -p ../ -f tree --tree-line-length 69 --tree-print-groupings $3 ../$1@*.yang > $1-groupings-tree.txt
+  cat $1-groupings-tree.txt | sed -n "/^  grouping $2/,/^  grouping/p" > tmp
+  c=$(grep -c "^  grouping" tmp)
+  if [ "$c" -ne "1" ]; then
+    ghead -n -1 tmp > $4
+    rm tmp
+  else
+    mv tmp $4
+  fi
+}
+
+extract_grouping() {
+  # $1 name of module
+  # $2 name of groupin
+  #extract_grouping_with_params "$1" "$2" "" "tree-$2.expanded.txt"
+  extract_grouping_with_params "$1" "$2" "--tree-no-expand-uses" "tree-$2.no-expand.txt"
+}
+
+extract_grouping ietf-tcp-common tcp-common-grouping
+extract_grouping ietf-tcp-common tcp-connection-grouping
+extract_grouping ietf-tcp-client tcp-client-grouping
+extract_grouping ietf-tcp-server tcp-server-grouping
 
